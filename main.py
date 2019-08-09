be0acecafe173fdd9f5d9dfdbf59315b435d9d62
ksy88 <53264655+ksy88@users.noreply.github.com>
2019-08-09 20:56:33 +0900

    web about language(ing)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_nav import *
+
 
 app = Flask(__name__)
+nav=
+
 
 @app.route("/")
 def home():
@@ -9,6 +13,18 @@ def home():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/chinese')
+def chinese():
+    return render_template("chinese.html")
+
+@app.route('/korean')
+def korean():
+    return render_template("korean.html")
+
+@app.route('/arabic')
+def arabic():
+    return render_template("arabic.html")
 
 if __name__=="__main__":
     app.run(debug=True)
